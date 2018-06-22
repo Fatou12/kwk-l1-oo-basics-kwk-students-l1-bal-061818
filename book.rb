1b15416ce4f36fc692c0e5803a5dec0c5ebe0d6e
Fatou12 <fatoumatadiallo12222@gmail.com>
2018-06-22 15:19:38 +0000

    automatically backed up by learn

--- book.rb
+++ book.rb
@@ -16,3 +16,17 @@ class book
 def author
   @author
 end
+
+def page_count=(num)
+  @page_count = num
+end
+
+def page_count
+  @page_count
+end
+
+def genre=(genre)
+  @genre = genre
+def genre
+  @genre
+end
